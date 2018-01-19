20ecf3c9a6e0eb31aeed8b5be8c53bf5b3f094c3
jovanialferez <vojalf@gmail.com>
2018-01-19 14:20:30 +0800

    Removing dusk from service providers.

--- config/app.php
+++ config/app.php
@@ -168,8 +168,6 @@ return [
          * Package Service Providers...
          */
 
-        Laravel\Dusk\DuskServiceProvider::class,
-        
         /*
          * Application Service Providers...
          */
